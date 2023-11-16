6a4e8b7a3086a01f5dd59fe3c6efb8bebf423ce3
Aidan Khoury <aidan@revers.engineering>
2023-11-16 00:19:19 -0400

    xhci: Add workaround for Intel PME quirk

--- hw/usb/hcd-xhci.h
+++ hw/usb/hcd-xhci.h
@@ -183,6 +183,7 @@ typedef struct XHCIState {
     MemoryRegion mem_oper;
     MemoryRegion mem_runtime;
     MemoryRegion mem_doorbell;
+    MemoryRegion mem_vendor;
 
     /* properties */
     uint32_t numports_2;
@@ -223,6 +224,7 @@ typedef struct XHCIState {
     XHCIRing cmd_ring;
 
     bool nec_quirks;
+    bool intel_quirks;
 } XHCIState;
 
 extern const VMStateDescription vmstate_xhci;
